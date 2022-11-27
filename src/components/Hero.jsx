1fbb72c1c9726fc5097cb0db8c3521ebe4150100
kenizaya <harveylstark@gmail.com>
2022-11-28 03:50:15 +0530

    Animate name and title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,44 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const Hero = () => {
+  const variants = {
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.3,
+      },
+    },
+    hidden: { opacity: 0 },
+  }
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: '50%' },
+    show: { opacity: 1, y: '0' },
+  }
   return (
     <div className='flex justify-center items-center w-full h-screen'>
-      <div className='w-full'>
-        <div className='font-valencia text-[150px] text-center'>
+      <motion.div
+        variants={variants}
+        initial='hidden'
+        animate='show'
+        className='w-full'
+      >
+        <motion.div
+          variants={itemVariants}
+          transition={{ duration: 0.5 }}
+          className='font-valencia text-[150px] text-center'
+        >
           Prashant Thapa
-        </div>
-        <div className='font-avenirRoman text-5xl text-center'>
+        </motion.div>
+        <motion.div
+          variants={itemVariants}
+          transition={{ duration: 0.75 }}
+          className='font-avenirRoman text-5xl text-center'
+        >
           Front-End Developer
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   )
 }
